549b11450de01666db7fb6335189833a656840fe
unknown <leixiaohua1020@126.com>
2015-01-09 16:18:14 +0800

    first commit

---
+++ simplest_ffmpeg_audio_player/ReadMe.txt
@@ -0,0 +1,27 @@
+﻿最简单的基于FFmpeg的音频播放器 2 (SDL 2.0)
+Simplest FFmpeg Audio Player 2 (SDL 2.0)
+
+该版本使用SDL 2.0替换了第一个版本中的SDL 1.0。
+注意：SDL 2.0中音频解码的API并无变化。唯一变化的地方在于
+其回调函数的中的Audio Buffer并没有完全初始化，需要手动初始化。
+本例子中即SDL_memset(stream, 0, len);
+
+This version use SDL 2.0 instead of SDL 1.2 in version 1
+Note:The good news for audio is that, with one exception, 
+it's entirely backwards compatible with 1.2.
+That one really important exception: The audio callback 
+does NOT start with a fully initialized buffer anymore. 
+You must fully write to the buffer in all cases. In this 
+example it is SDL_memset(stream, 0, len);
+
+雷霄骅 Lei Xiaohua
+leixiaohua1020@126.com
+中国传媒大学/数字电视技术
+Communication University of China / Digital TV Technology
+http://blog.csdn.net/leixiaohua1020
+
+本程序实现了音频的解码和播放。
+
+This software decode and play audio streams.
+
+Version 2.0
---
+++ simplest_ffmpeg_audio_player/simplest_ffmpeg_audio_player.cpp
@@ -0,0 +1,260 @@
+/**
+ * 最简单的基于FFmpeg的音频播放器 2 (SDL 2.0)
+ * Simplest FFmpeg Audio Player 2 (SDL 2.0)
+ *
+ * 该版本使用SDL 2.0替换了第一个版本中的SDL 1.0。
+ * 注意：SDL 2.0中音频解码的API并无变化。唯一变化的地方在于
+ * 其回调函数的中的Audio Buffer并没有完全初始化，需要手动初始化。
+ * 本例子中即SDL_memset(stream, 0, len);
+ * 
+ * This version use SDL 2.0 instead of SDL 1.2 in version 1
+ * Note:The good news for audio is that, with one exception, 
+ * it's entirely backwards compatible with 1.2.
+ * That one really important exception: The audio callback 
+ * does NOT start with a fully initialized buffer anymore. 
+ * You must fully write to the buffer in all cases. In this 
+ * example it is SDL_memset(stream, 0, len);
+ *
+ * 雷霄骅 Lei Xiaohua
+ * leixiaohua1020@126.com
+ * 中国传媒大学/数字电视技术
+ * Communication University of China / Digital TV Technology
+ * http://blog.csdn.net/leixiaohua1020
+ *
+ * 本程序实现了音频的解码和播放。
+ *
+ * This software decode and play audio streams.
+ *
+ * Version 2.0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define __STDC_CONSTANT_MACROS
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswresample/swresample.h"
+//SDL
+#include "sdl/SDL.h"
+#include "sdl/SDL_thread.h"
+};
+
+#define MAX_AUDIO_FRAME_SIZE 192000 // 1 second of 48khz 32bit audio
+
+//Output PCM
+#define OUTPUT_PCM 1
+//Use SDL
+#define USE_SDL 1
+
+//Buffer:
+//|-----------|-------------|
+//chunk-------pos---len-----|
+static  Uint8  *audio_chunk; 
+static  Uint32  audio_len; 
+static  Uint8  *audio_pos; 
+
+/* The audio function callback takes the following parameters: 
+ * stream: A pointer to the audio buffer to be filled 
+ * len: The length (in bytes) of the audio buffer 
+ * 回调函数
+*/ 
+void  fill_audio(void *udata,Uint8 *stream,int len){ 
+	//SDL 2.0
+	SDL_memset(stream, 0, len);
+	if(audio_len==0)		/*  Only  play  if  we  have  data  left  */ 
+			return; 
+	len=(len>audio_len?audio_len:len);	/*  Mix  as  much  data  as  possible  */ 
+
+	SDL_MixAudio(stream,audio_pos,len,SDL_MIX_MAXVOLUME);
+	audio_pos += len; 
+	audio_len -= len; 
+} 
+//-----------------
+
+
+int main(int argc, char* argv[])
+{
+	AVFormatContext	*pFormatCtx;
+	int				i, audioStream;
+	AVCodecContext	*pCodecCtx;
+	AVCodec			*pCodec;
+
+	char url[]="WavinFlag.aac";
+
+	av_register_all();
+	avformat_network_init();
+	pFormatCtx = avformat_alloc_context();
+	//Open
+	if(avformat_open_input(&pFormatCtx,url,NULL,NULL)!=0){
+		printf("Couldn't open input stream.\n");
+		return -1;
+	}
+	// Retrieve stream information
+	if(avformat_find_stream_info(pFormatCtx,NULL)<0){
+		printf("Couldn't find stream information.\n");
+		return -1;
+	}
+	// Dump valid information onto standard error
+	av_dump_format(pFormatCtx, 0, url, false);
+
+	// Find the first audio stream
+	audioStream=-1;
+	for(i=0; i < pFormatCtx->nb_streams; i++)
+		if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_AUDIO){
+			audioStream=i;
+			break;
+		}
+
+	if(audioStream==-1){
+		printf("Didn't find a audio stream.\n");
+		return -1;
+	}
+
+	// Get a pointer to the codec context for the audio stream
+	pCodecCtx=pFormatCtx->streams[audioStream]->codec;
+
+	// Find the decoder for the audio stream
+	pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+	if(pCodec==NULL){
+		printf("Codec not found.\n");
+		return -1;
+	}
+
+	// Open codec
+	if(avcodec_open2(pCodecCtx, pCodec,NULL)<0){
+		printf("Could not open codec.\n");
+		return -1;
+	}
+
+	FILE *pFile=NULL;
+#if OUTPUT_PCM
+	pFile=fopen("output.pcm", "wb");
+#endif
+
+	AVPacket *packet=(AVPacket *)av_malloc(sizeof(AVPacket));
+	av_init_packet(packet);
+
+	//Out Audio Param
+	uint64_t out_channel_layout=AV_CH_LAYOUT_STEREO;
+	int out_nb_samples=1024;
+	AVSampleFormat out_sample_fmt=AV_SAMPLE_FMT_S16;
+	int out_sample_rate=44100;
+	int out_channels=av_get_channel_layout_nb_channels(out_channel_layout);
+	//Out Buffer Size
+	int out_buffer_size=av_samples_get_buffer_size(NULL,out_channels ,out_nb_samples,out_sample_fmt, 1);
+
+	uint8_t *out_buffer=(uint8_t *)av_malloc(MAX_AUDIO_FRAME_SIZE*2);
+
+	AVFrame	*pFrame;
+	pFrame=avcodec_alloc_frame();
+//SDL------------------
+#if USE_SDL
+	//Init
+	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {  
+		printf( "Could not initialize SDL - %s\n", SDL_GetError()); 
+		return -1;
+	}
+	//SDL_AudioSpec
+	SDL_AudioSpec wanted_spec;
+	wanted_spec.freq = out_sample_rate; 
+	wanted_spec.format = AUDIO_S16SYS; 
+	wanted_spec.channels = out_channels; 
+	wanted_spec.silence = 0; 
+	wanted_spec.samples = out_nb_samples; 
+	wanted_spec.callback = fill_audio; 
+	wanted_spec.userdata = pCodecCtx; 
+
+	if (SDL_OpenAudio(&wanted_spec, NULL)<0){ 
+		printf("can't open audio.\n"); 
+		return -1; 
+	} 
+#endif
+	printf("Bitrate:\t %3d\n", pFormatCtx->bit_rate);
+	printf("Decoder Name:\t %s\n", pCodecCtx->codec->long_name);
+	printf("Channels:\t %d\n", pCodecCtx->channels);
+	printf("Sample per Second\t %d \n", pCodecCtx->sample_rate);
+
+	uint32_t ret,len = 0;
+	int got_picture;
+	int index = 0;
+	//FIX:Some Codec's Context Information is missing
+	int64_t in_channel_layout=av_get_default_channel_layout(pCodecCtx->channels);
+	//Swr
+	struct SwrContext *au_convert_ctx;
+	au_convert_ctx = swr_alloc();
+	au_convert_ctx=swr_alloc_set_opts(au_convert_ctx,out_channel_layout, out_sample_fmt, out_sample_rate,
+		in_channel_layout,pCodecCtx->sample_fmt , pCodecCtx->sample_rate,0, NULL);
+	swr_init(au_convert_ctx);
+	while(av_read_frame(pFormatCtx, packet)>=0){
+		if(packet->stream_index==audioStream){
+
+			ret = avcodec_decode_audio4( pCodecCtx, pFrame,&got_picture, packet);
+			if ( ret < 0 ) {
+                printf("Error in decoding audio frame.\n");
+                return -1;
+            }
+			if ( got_picture > 0 ){
+				swr_convert(au_convert_ctx,&out_buffer, MAX_AUDIO_FRAME_SIZE,(const uint8_t **)pFrame->data , pFrame->nb_samples);
+#if 1
+				printf("index:%5d\t pts:%I64d\t packet size:%d\n",index,packet->pts,packet->size);
+#endif
+
+#if USE_SDL
+				//FIX:FLAC,MP3,AAC Different number of samples
+				if(wanted_spec.samples!=pFrame->nb_samples){
+					SDL_CloseAudio();
+					out_nb_samples=pFrame->nb_samples;
+					out_buffer_size=av_samples_get_buffer_size(NULL,out_channels ,out_nb_samples,out_sample_fmt, 1);
+					wanted_spec.samples=out_nb_samples;
+					SDL_OpenAudio(&wanted_spec, NULL);
+				}
+#endif
+
+#if OUTPUT_PCM
+				//Write PCM
+				fwrite(out_buffer, 1, out_buffer_size, pFile);
+#endif
+				
+				index++;
+			}
+//SDL------------------
+#if USE_SDL
+			//Set audio buffer (PCM data)
+			audio_chunk = (Uint8 *) out_buffer; 
+			//Audio buffer length
+			audio_len =out_buffer_size;
+
+			audio_pos = audio_chunk;
+			//Play
+			SDL_PauseAudio(0);
+			while(audio_len>0)//Wait until finish
+				SDL_Delay(1); 
+#endif
+		}
+		av_free_packet(packet);
+	}
+
+	swr_free(&au_convert_ctx);
+
+#if USE_SDL
+	SDL_CloseAudio();//Close SDL
+	SDL_Quit();
+#endif
+	// Close file
+#if OUTPUT_PCM
+	fclose(pFile);
+#endif
+	av_free(out_buffer);
+	// Close the codec
+	avcodec_close(pCodecCtx);
+	// Close the video file
+	avformat_close_input(&pFormatCtx);
+
+	return 0;
+}
+
+
