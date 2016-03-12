f4f290730562e7a44136fa256598fbf450b1a857
Lei Xiaohua <leixiaohua1020@126.com>
2016-03-12 12:30:46 +0800

    remove some text

--- simplest_audio_play_sdl2/compile_cl.bat
+++ simplest_audio_play_sdl2/compile_cl.bat
@@ -1,12 +1,3 @@
-::最简单的SDL2播放音频的例子（SDL2播放PCM） ----命令行编译
-::Simplest Audio Play SDL2 (SDL2 play PCM) ----Compile in Cmd 
-::
-::雷霄骅 Lei Xiaohua
-::leixiaohua1020@126.com
-::中国传媒大学/数字电视技术
-::Communication University of China / Digital TV Technology
-::http://blog.csdn.net/leixiaohua1020
-::
 ::VS2010 Environment
 call "D:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 ::include
